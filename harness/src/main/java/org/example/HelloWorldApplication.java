6e37f72d5dc445196743cc8fd5f9168c63979148
krishna Prasad <krishna.prasad@xebia.com>
2022-12-28 08:05:13 +0530

    first commit

--- harness/src/main/java/org/example/HelloWorldApplication.java
+++ harness/src/main/java/org/example/HelloWorldApplication.java
@@ -5,14 +5,33 @@ import io.dropwizard.Configuration;
 import io.dropwizard.setup.Environment;
 import org.example.config.HelloWorldConfiguration;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+
 public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
 
     public static void main(String[] args) throws Exception {
         new HelloWorldApplication().run(args);
-        
+
     }
     @Override
     public void run(HelloWorldConfiguration configuration, Environment environment) throws Exception {
+        File directory = new File("/app/files");
+        File directory1 =  new File(directory.getPath()+File.separator+"files");
+        System.out.println("Hello World:-->" +directory.exists());
+        System.out.println("Hello World:-->" +directory1.exists());
+        System.out.println("Hello World:-->" +directory1.mkdirs());
+        System.out.println("Hello World:-->" +directory1.getPath());
+
+            FileOutputStream fileOut = new FileOutputStream(directory1+"/hello.ser", true);
+            OutputStreamWriter out = new OutputStreamWriter(fileOut, StandardCharsets.UTF_8);
+            StringBuilder builder = new StringBuilder();
+            String sequenceNo = "hello";
+
+            out.write(builder.toString());
+            out.close();
         System.out.println("Hello World:-->" +configuration.getGreeting());
     }
 }
